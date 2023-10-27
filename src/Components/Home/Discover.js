28e1e87eb66987861ca89d103a449308231ce3f7
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-10-27 19:15:22 +0300

    added discover

--- src/Components/Home/Discover.js
+++ src/Components/Home/Discover.js
@@ -1,12 +1,89 @@
 import React from 'react'
+import Image from 'next/image';
 
 function Discover() {
   return (
+    <>
     <div>
         <div>
             <h3>Discover</h3>
         </div>
+        <div className="discover">
+            <div className='discoverCard'>
+                <div className='intro-line'>
+                    <p>
+                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
+                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
+                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
+                    </p>
+                </div>
+                <div className='pic'>
+                    <Image 
+                        src="/image/massai1.png" 
+                        alt="user" 
+                        width={150} 
+                        height={150} 
+                        className='img' />
+                </div>
+            
+            </div>
+            <div className='discoverCard'>
+                <div className='intro-line'>
+                    <p>
+                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
+                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
+                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
+                    </p>
+                </div>
+                <div className='pic'>
+                    <Image 
+                        src="/image/massai1.png" 
+                        alt="user" 
+                        width={150} 
+                        height={150} 
+                        className='img' />
+                </div>
+            
+            </div>
+            <div className='discoverCard'>
+                <div className='intro-line'>
+                    <p>
+                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
+                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
+                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
+                    </p>
+                </div>
+                <div className='pic'>
+                    <Image 
+                        src="/image/massai1.png" 
+                        alt="user" 
+                        width={150} 
+                        height={150} 
+                        className='img' />
+                </div>
+            
+            </div>
+            <div className='discoverCard'>
+                <div className='intro-line'>
+                    <p>
+                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
+                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
+                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
+                    </p>
+                </div>
+                <div className='pic'>
+                    <Image 
+                        src="/image/massai1.png" 
+                        alt="user" 
+                        width={150} 
+                        height={150} 
+                        className='img' />
+                </div>
+            
+            </div>
+        </div>
     </div>
+    </>
   )
 }
 
