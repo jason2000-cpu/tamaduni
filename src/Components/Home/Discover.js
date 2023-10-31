eba1d214addb6fc4d0d148219beac7a7f9ee9a76
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-10-31 12:57:53 +0300

    perfected discover card

--- src/Components/Home/Discover.js
+++ src/Components/Home/Discover.js
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Image from 'next/image';
 
 function Discover() {
+
+    useEffect(() => {
+        const link = document.createElement('link');
+        link.href = 'https://fonts.googleapis.com/css?family=Font1|Font2';
+        link.rel = 'stylesheet';
+        document.head.appendChild(link);
+    
+        // Clean up the link element when the component unmounts
+        return () => {
+          document.head.removeChild(link);
+        };
+      }, []);
+
   return (
     <>
     <div>
         <div>
-            <h3>Discover</h3>
+            <h3 style={{ fontFamily: '' }}>Discover</h3>
         </div>
         <div className="discover">
                 <div className='discoverCard'>
-                    <div>
-                        <h3>Maasai</h3>
-                    </div>
+                    <div >
+                        <h3 style={{paddingTop:"10px"}}> The Maasai</h3>
+                    </div>  
                     <div className='info'>
                         <div className='intro-line'>
                             <p>
-                                A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
-                                is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
-                                shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
+                            A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
+                            is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
+                            shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing. 
+                            These vibrant and eye-catching outfits are not only practical for the Maasai's pastoral lifestyle but have also become iconic 
+                            symbols of their culture and are popular with tourists and fashion enthusiasts around the world.
                             </p>
                             <div style={{paddingLeft:"10px",  display:"flex", alignContent:"flex-end", height:"40px", marginBottom:"2px"}}>
                                 <p>Learn More...</p>
@@ -27,9 +42,10 @@ function Discover() {
                         <div className='pic'>
                             <Image 
                                 src="/image/massai1.png" 
+                                // style={{border: "1px solid black"}}
                                 alt="user" 
-                                width={150} 
-                                height={150} 
+                                width={200} 
+                                height={200} 
                                 className='img' />
                         </div>
                     </div>
