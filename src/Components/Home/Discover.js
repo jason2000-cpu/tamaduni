d6e3c8f1af226932939774952f172bc63e1b9784
jason2000-cpu <jasonmwangi2000@gmail.com>
2023-10-31 10:42:27 +0300

    perfected discover card

--- src/Components/Home/Discover.js
+++ src/Components/Home/Discover.js
@@ -9,78 +9,31 @@ function Discover() {
             <h3>Discover</h3>
         </div>
         <div className="discover">
-            <div className='discoverCard'>
-                <div className='intro-line'>
-                    <p>
-                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
-                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
-                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
-                    </p>
+                <div className='discoverCard'>
+                    <div>
+                        <h3>Maasai</h3>
+                    </div>
+                    <div className='info'>
+                        <div className='intro-line'>
+                            <p>
+                                A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
+                                is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
+                                shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
+                            </p>
+                            <div style={{paddingLeft:"10px",  display:"flex", alignContent:"flex-end", height:"40px", marginBottom:"2px"}}>
+                                <p>Learn More...</p>
+                            </div>
+                        </div>
+                        <div className='pic'>
+                            <Image 
+                                src="/image/massai1.png" 
+                                alt="user" 
+                                width={150} 
+                                height={150} 
+                                className='img' />
+                        </div>
+                    </div>
                 </div>
-                <div className='pic'>
-                    <Image 
-                        src="/image/massai1.png" 
-                        alt="user" 
-                        width={150} 
-                        height={150} 
-                        className='img' />
-                </div>
-            
-            </div>
-            <div className='discoverCard'>
-                <div className='intro-line'>
-                    <p>
-                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
-                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
-                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
-                    </p>
-                </div>
-                <div className='pic'>
-                    <Image 
-                        src="/image/massai1.png" 
-                        alt="user" 
-                        width={150} 
-                        height={150} 
-                        className='img' />
-                </div>
-            
-            </div>
-            <div className='discoverCard'>
-                <div className='intro-line'>
-                    <p>
-                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
-                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
-                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
-                    </p>
-                </div>
-                <div className='pic'>
-                    <Image 
-                        src="/image/massai1.png" 
-                        alt="user" 
-                        width={150} 
-                        height={150} 
-                        className='img' />
-                </div>
-            
-            </div>
-            <div className='discoverCard'>
-                <div className='intro-line'>
-                    <p>
-                    A fun fact about the Maasai people, who are a semi-nomadic ethnic group in East Africa (Kenya and Tanzania), 
-                    is that they are known for their distinctive and colorful clothing. Maasai men often wear a traditional red 
-                    shuka (a type of cloth) wrapped around their bodies, while women typically wear brightly colored beaded jewelry and clothing....
-                    </p>
-                </div>
-                <div className='pic'>
-                    <Image 
-                        src="/image/massai1.png" 
-                        alt="user" 
-                        width={150} 
-                        height={150} 
-                        className='img' />
-                </div>
-            
-            </div>
         </div>
     </div>
     </>
